8a44a4c169e7e769944649218584ba60dfca6e40
tenzin <ten13zin@gmail.com>
2021-02-22 15:23:40 +0530

    add: add text modal

---
+++ src/components/Modals/AddText.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-card>
+    <q-card-section class="row">
+      <div class="text-h6">Upload Text</div>
+      <q-space />
+      <q-btn v-close-popup flat round dense icon="close" />
+    </q-card-section>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          (val) => (val !== null && val !== '') || 'Please type your age',
+          (val) => (val > 0 && val < 100) || 'Please type a real age',
+        ]"
+      />
+
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
+  </q-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pechaSecretKey: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log("submiteFrom method");
+      this.$emit("close");
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

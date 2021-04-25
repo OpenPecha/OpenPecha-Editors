88dfb1b10c9e34e85727f1c92705d20dfe189ba1
tenzin <ten13zin@gmail.com>
2021-04-26 01:04:11 +0530

    fix: load page text

---
+++ src/components/Pedurma/Editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="full-width">
+    <q-input v-model="text" type="textarea" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>

4c73ffc2e05e8de90e306a59c94e3986178cd234
tenzin <ten13zin@gmail.com>
2021-08-12 21:39:26 +0530

    fix(proofread): call api in action to fetch data

--- src/store/proofread/actions.js
+++ src/store/proofread/actions.js
@@ -7,14 +7,7 @@ export async function fetchVolumes({commit}, {pechaId}) {
 
 
 export async function fetchPageIds({commit, dispatch}, {pechaId, volId}) {
-    // const response = await axios.get(getOrigin() + `/api/v1/proofread/${pechaId}/${volId}`)
-    const response = {
-        data: [
-            "0005",
-            "0006",
-            "0007",
-        ]
-    }
+    const response = await axios.get(process.env.OPENPECHA_API_URL + `/api/v1/proofread/${pechaId}/${volId}`)
     commit("setPageIds", response.data)
 
     // prefetch first page
@@ -22,14 +15,8 @@ export async function fetchPageIds({commit, dispatch}, {pechaId, volId}) {
 }
 
 export async function fetchPage({commit}, {pechaId, volId, pageId}) {
-    // const response = await axios.get(getOrigin() + `/api/v1/proofread/${pechaId}/${volId}/${pageId}`)
-    console.log(pechaId, volId, pageId)
-    const response = {
-        data: {
-            content: `this page ${pageId} content`,
-            "image_url": `https://iiif.bdrc.io/bdr:I0886::0886${pageId}.tif/full/max/0/default.png`
-        }
-    }
+    const response = await axios.get(process.env.OPENPECHA_API_URL +  `/api/v1/proofread/${pechaId}/${volId}/${pageId}`)
+
     commit("setPageContent", response.data.content)
     commit("setPageImageUrl", response.data["image_url"])
 }

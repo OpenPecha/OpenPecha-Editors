1f9c3261c79b53d76a89a836319ddd0a76154172
tenzin <ten13zin@gmail.com>
2021-11-05 16:19:55 +0530

    fix(pedurma): set error to true on failed img load

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -3,15 +3,14 @@
     v-if="!error"
     :src="srcSmall"
     :zoomSrc="src"
-    alt="Login into bdrc.io to view the image"
-    :onerror="error = true"
   />
-  <div v-else class="column items-center q-gutter-md">
+
+  <div v-else class="column items-center">
     <q-icon name="error" size="xl" class="text-grey" />
-    <div class="text-grey">
-      <div class="text-h5">To view the image</div>
+    <div class="text-grey q-mt-md">
+      <div class="text-h5 text-center">To view the image</div>
       <ol>
-        <li>Login into <a href="https://library.bdrc.io" target="_blank">BDRC library</a></li>
+        <li>Login into <a href="https://library.bdrc.io" target="_blank">BDRC library</a> with an authorized account</li>
         <li>Reload the page</li>
       </ol>
     </div>
@@ -53,6 +52,13 @@ export default {
       return splitedURL[0] + "pct:50" + splitedURL[1];
     },
   },
+
+  mounted() {
+    const img = document.querySelector(".iiz__img")
+    img.addEventListener("error", err => {
+      this.error = true;
+    })
+  }
 };
 </script>
 
